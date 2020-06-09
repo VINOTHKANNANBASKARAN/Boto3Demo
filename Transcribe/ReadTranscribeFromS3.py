922f032fe7bc46334df1f896b32568bdb994ceca
VINOTH KANNAN <bvinothkannan@yahoo.com>
2020-06-09 22:34:14 +0530

    Adding Transcribe- reading transcripts from s3 using boto s3client and adding response to the file for reference

--- Transcribe/ReadTranscribeFromS3.py
+++ Transcribe/ReadTranscribeFromS3.py
@@ -5,7 +5,9 @@ FILE_TO_READ = 'transcribefromboto3trial3.json'
 s3 = boto3.resource('s3')
 
 content_object = s3.Object(BUCKET, FILE_TO_READ)
+
 file_content = content_object.get()['Body'].read().decode('utf-8')
+print(file_content)
 json_content = json.loads(file_content)
 print(json_content)
 print(json_content['results']['transcripts'][0]['transcript'])
